f07015d0882f867a31831163b4bb91c716873447
akadirkaratas <a.kadir.karatass@gmail.com>
2026-03-15 13:23:09 +0300

    Ogrenci/Edit ve Kurs/Edit sayfalarinda Kurs List ve Ogrenci List gorunur oldu

--- Data/Ogrenci.cs
+++ Data/Ogrenci.cs
@@ -19,5 +19,7 @@ namespace efcoreApp.Data
          }
         public string? Eposta { get; set; }
         public string? Telefon { get; set; }
+
+        public ICollection<KursKayit> KursKayitlari { get; set; } = new List<KursKayit>();
     }
 }
